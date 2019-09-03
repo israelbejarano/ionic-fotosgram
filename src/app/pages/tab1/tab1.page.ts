60795c32ef8e3cf141339db34dc94524e444ae45
israel <israel.bejarano.a@gmail.com>
2019-09-03 17:26:10 +0200

    infinite scroll

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -14,9 +14,20 @@ export class Tab1Page implements OnInit {
   constructor(private postsService: PostService) {}
 
   ngOnInit() {
+    this.siguientes();
+  }
+
+  siguientes(event?) {
     this.postsService.getPosts().subscribe((resp: RespuestaPosts) => {
       console.log(resp);
       this.posts.push(...resp.posts);
+      if (event) {
+        event.target.complete();
+        // desactivar el infinite scroll para evitar la carga cuando llegue el final
+        if (resp.posts.length === 0) {
+          event.target.disabled = true;
+        }
+      }
     });
   }
 
