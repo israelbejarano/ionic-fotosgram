43a0b2ec0e6b37b77ed2e65943ecf2cf4e9d4746
israel <israel.bejarano.a@gmail.com>
2019-09-03 14:02:55 +0200

    servicio para traer los posts

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { RespuestaPosts } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(private postsService: PostService) {}
+
+  ngOnInit() {
+    this.postsService.getPosts().subscribe((resp: RespuestaPosts) => {
+      console.log(resp);
+    });
+  }
 
 }
