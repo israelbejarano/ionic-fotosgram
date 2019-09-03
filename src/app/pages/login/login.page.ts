f1c847f8bfec0640f1b07b2d38b9819cf924e4b9
israel <israel.bejarano.a@gmail.com>
2019-09-03 23:05:17 +0200

    login para que devuelva el token

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { IonSlides } from '@ionic/angular';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -50,14 +51,21 @@ export class LoginPage implements OnInit {
     slidesPerView: 3.5
   };
 
-  constructor() { }
+  loginUser = {
+    email: 'israel@prueba.com',
+    password: '123456'
+  };
+
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
     this.slides.lockSwipes(true);
   }
 
   login(fLogin: NgForm) {
-    console.log(fLogin.valid);
+    if (fLogin.invalid) {return;}
+    this.usuarioService.login(this.loginUser.email, this.loginUser.password);
+    console.log(this.loginUser);
   }
 
   registro(fRegistro: NgForm) {
