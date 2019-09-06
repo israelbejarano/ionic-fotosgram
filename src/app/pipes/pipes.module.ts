ec5dcebfa2e30a5f2d598d08e4fea532a192e365
israel <israel.bejarano.a@gmail.com>
2019-09-06 13:44:34 +0200

    imagen pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { DomSanitizerPipe } from './dom-sanitizer.pipe';
 import { ImageSanitizerPipe } from './image-sanitizer.pipe';
+import { ImagenPipe } from './imagen.pipe';
 
 
 
 @NgModule({
-  declarations: [DomSanitizerPipe, ImageSanitizerPipe],
+  declarations: [DomSanitizerPipe, ImageSanitizerPipe, ImagenPipe],
   exports: [
     DomSanitizerPipe,
-    ImageSanitizerPipe
+    ImageSanitizerPipe,
+    ImagenPipe
   ]
 })
 export class PipesModule { }
