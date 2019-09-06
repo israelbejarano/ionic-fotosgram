207ffa4f37adbf047c914b7ff3c97672bb91c0d4
israel <israel.bejarano.a@gmail.com>
2019-09-06 13:09:58 +0200

    uso de la galeria mediante el plugin camera

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -65,6 +65,23 @@ export class Tab2Page {
       sourceType: this.camera.PictureSourceType.CAMERA
     };
 
+    this.procesarImagen(options);
+  }
+
+  libreria() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    this.procesarImagen(options);
+  }
+
+  procesarImagen(options: CameraOptions) {
     this.camera.getPicture(options).then((imageData) => {
      // imageData is either a base64 encoded string or a file URI
      // If it's base64 (DATA_URL):
