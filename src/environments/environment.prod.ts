76af8d5332d51c244eb5af3eeb282f048797ef8b
israel <israel.bejarano.a@gmail.com>
2019-09-06 12:47:29 +0200

    cambio de url para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:3000'
+  url: 'http://localhost:3000' // para probar la camara y demás tener en cuenta cambiar la ip a la del servidor
 };
